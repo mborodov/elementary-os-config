64c0656d9709015fd08f7225a5dc72e6bfa8336c
Romain Catajar <rcatajar@gmail.com>
2015-10-11 22:59:11 +0200

    add dotfiles script

---
+++ dotfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Author: Romain Catajar <rcatajar@gmail.com>
+#
+# Retrieve and copy my dotfiles
+
+# GIT
+wget https://raw.githubusercontent.com/rcatajar/elementary-os-config/master/data/gitconfig
+mv gitconfig ~/.gitconfig
+
